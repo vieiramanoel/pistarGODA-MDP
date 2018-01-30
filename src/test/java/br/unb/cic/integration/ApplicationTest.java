4f7b8b1e92b36dc56183641b7bdfd84be7e58a39
Leandro Bergmann <leandro.bergmann@hotmail.com>
2018-01-30 10:56:04 -0200

    Adjusting tests and testing Heroku deploy

--- src/test/java/br/unb/cic/integration/ApplicationTest.java
+++ src/test/java/br/unb/cic/integration/ApplicationTest.java
@@ -1,5 +1,6 @@
 package br.unb.cic.integration;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,246 +29,419 @@ public class ApplicationTest {
     @Test
     public void testCase1() throws Exception {
         String content = getContent("Test1.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+        } catch (Exception e) {
+            Assert.fail();
+        }
     }
 
     @Test
     public void testCase2() throws Exception {
         String content = getContent("Test2.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+        } catch (Exception e) {
+            Assert.fail();
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase3() throws Exception {
         String content = getContent("Test3.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase4() throws Exception {
         String content = getContent("Test4.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase5() throws Exception {
         String content = getContent("Test5.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase6() throws Exception {
         String content = getContent("Test6.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase7() throws Exception {
         String content = getContent("Test7.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase8() throws Exception {
         String content = getContent("Test8.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase9() throws Exception {
         String content = getContent("Test9.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase10() throws Exception {
         String content = getContent("Test10.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase11() throws Exception {
         String content = getContent("Test11.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase12() throws Exception {
         String content = getContent("Test12.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase13() throws Exception {
         String content = getContent("Test13.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase14() throws Exception {
         String content = getContent("Test14.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase15() throws Exception {
         String content = getContent("Test15.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase16() throws Exception {
         String content = getContent("Test16.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase17() throws Exception {
         String content = getContent("Test17.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase18() throws Exception {
         String content = getContent("Test18.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase19() throws Exception {
         String content = getContent("Test19.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase20() throws Exception {
         String content = getContent("Test20.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase21() throws Exception {
         String content = getContent("Test21.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase22() throws Exception {
         String content = getContent("Test22.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase23() throws Exception {
         String content = getContent("Test23.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase24() throws Exception {
         String content = getContent("Test24.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase25() throws Exception {
         String content = getContent("Test25.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase26() throws Exception {
         String content = getContent("Test26.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase27() throws Exception {
         String content = getContent("Test27.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase28() throws Exception {
         String content = getContent("Test28.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase29() throws Exception {
         String content = getContent("Test29.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase30() throws Exception {
         String content = getContent("Test30.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase31() throws Exception {
         String content = getContent("Test31.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase32() throws Exception {
         String content = getContent("Test32.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase33() throws Exception {
         String content = getContent("Test33.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase34() throws Exception {
         String content = getContent("Test34.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
-    @Test(expected = Exception.class)
+    @Test
     public void testCase35() throws Exception {
         String content = getContent("Test35.txt");
-        mockMvc.perform(post("/goda").param("content", content))
-                .andExpect(status().isOk());
+        try {
+            mockMvc.perform(post("/goda").param("content", content))
+                    .andExpect(status().isOk());
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertTrue(true);
+        }
     }
 
 }
