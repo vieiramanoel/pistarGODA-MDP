4ce0ce329223998f0d6a1e80d12c9b296f8d77ee
Rafael Souza <rafael.souza@spread.com.br>
2020-10-08 15:10:49 -0300

    atualizacao pistar

--- src/main/webapp/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    istar.setupModel();
-    istar.setupDiagram();
-    istar.setupMetamodel(istarcoreMetamodel);
-    ui.defineInteractions();
-    examples.pistarIntro();
-    ui.setupUi();
-});
